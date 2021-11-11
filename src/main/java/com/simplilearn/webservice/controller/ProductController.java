732e7a4f452ba25f76b050dbd81eb90db36ef853
Sofia Nayak <nayaksofia1989@gmail.com>
2021-11-11 21:05:42 +0530

    Update and Delete Operation along with TODO

--- src/main/java/com/simplilearn/webservice/controller/ProductController.java
+++ src/main/java/com/simplilearn/webservice/controller/ProductController.java
@@ -3,9 +3,11 @@ package com.simplilearn.webservice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +43,35 @@ public class ProductController {
 		
 		return this.productRepository.save(product);
 	}
-
+    
+	//Update product
+	@PutMapping("/products/{id}")
+	public EProduct updateProduct(@RequestBody EProduct product) {
+		
+		//1. find product
+		EProduct fetchedProduct = this.productRepository.findById(product.getId()).orElseThrow(()->{
+			throw new ProductNotFoundException("Product Not Found With Id " + product.getId());
+		});
+		
+		//2. set new values
+		fetchedProduct.setName(product.getName());
+		fetchedProduct.setPrice(product.getPrice());
+		fetchedProduct.setDescription(product.getDescription());
+		
+		//3. save product
+		return this.productRepository.save(fetchedProduct);
+	}
+	
+	//Delete Product
+	@DeleteMapping("/products/{id}")
+	public void deleteProduct(@PathVariable(value="id") long productId) {
+		
+		//1. find product
+		EProduct fetchedProduct = this.productRepository.findById(productId).orElseThrow(()->{
+			throw new ProductNotFoundException("Product Not Found With Id " + productId);
+		});
+		
+		this.productRepository.delete(fetchedProduct);
+		
+	}
 }
---
+++ src/main/java/com/simplilearn/webservice/entity/User.java
@@ -0,0 +1,9 @@
+package com.simplilearn.webservice.entity;
+
+public class User {
+
+	//TODO :: WAP for CRUD Operations on user entity
+	//Example: id, name,email,gender , username, age etc..
+	
+	//Along with proper Exception Handling 
+}
